2f18f569d3c10a73a1257b851e697ccad6acb9ea
min E <66760972+mdobinda@users.noreply.github.com>
2022-03-19 13:56:36 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>don't care bozo</h3>
+      <h4> AWESOME</h4>
+    </div>
+  );
+}
